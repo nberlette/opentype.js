9e06b0bf26b2e0ab870e55c6c92b8d4a6630f81a
Nicholas Berlette <nick@berlette.com>
2023-03-05 16:29:29 -0800

    types: improve jsdoc types in check.js

--- src/check.js
+++ src/check.js
@@ -1,16 +1,27 @@
 // Run-time checking of preconditions.
-
-function fail(message) {
-  throw new Error(message);
+/**
+ * @param {string} [message]
+ * @param {ErrorConstructor|TypeErrorConstructor|SyntaxErrorConstructor|ReferenceErrorConstructor|AggregateErrorConstructor|EvalErrorConstructor} [type=Error] The type of error to throw. Defaults to `Error`.
+ * @returns {never} Always throws an error.
+ * @throws {Error} Alwaus throws an error with the given message.
+ */
+export function fail(message, type = Error) {
+  throw new (type instanceof Error ? type : Error)(message);
 }
 
-// Precondition function that checks if the given predicate is true.
-// If not, it will throw an error.
-function argument(predicate, message) {
-  if (!predicate) {
-    fail(message);
-  }
+/**
+ * Precondition function that checks if the given predicate is true.
+ * If not, it will throw an error.
+ *
+ * @param {boolean} predicate
+ * @param {string} message
+ * @returns {asserts predicate}
+ * @throws {Error} if the predicate is false or a falsy value.
+ */
+export function assert(predicate, message) {
+  if (!predicate) fail(message);
 }
 
-export { argument, argument as assert, fail };
-export default { fail, argument, assert: argument };
+export const argument = assert;
+
+export default { fail, argument, assert };
